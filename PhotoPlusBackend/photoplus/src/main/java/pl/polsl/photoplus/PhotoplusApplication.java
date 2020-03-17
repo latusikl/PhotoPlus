8592fa2f8aab0aa7fe434e48f056fab6615b953f
Lukasz Latusik <lukasz.latusik@sap.com>
2020-03-17 21:56:25 +0100

    Generated empty project

---
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/PhotoplusApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.photoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoplusApplication.class, args);
+	}
+
+}
---
+++ PhotoPlusBackend/photoplus/src/test/java/pl/polsl/photoplus/PhotoplusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.photoplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
