05c3db5dc948997228c32054db8368cae2c530ab
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-03-28 16:42:46 +0100

    Fixed OrderItemService

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/services/controllers/OrderItemService.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/services/controllers/OrderItemService.java
@@ -1,14 +1,28 @@
 package pl.polsl.photoplus.services.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import pl.polsl.photoplus.model.dto.OrderItemModelDto;
+import pl.polsl.photoplus.model.entities.Order;
 import pl.polsl.photoplus.model.entities.OrderItem;
+import pl.polsl.photoplus.model.entities.Product;
 import pl.polsl.photoplus.repositories.OrderItemRepository;
+
+import java.util.Set;
+import java.util.function.Function;
 
 @Service
 public class OrderItemService extends AbstractModelService<OrderItem, OrderItemModelDto, OrderItemRepository> {
-    public OrderItemService(final OrderItemRepository entityRepository) {
+
+    private OrderService orderService;
+
+    private ProductService productService;
+
+    public OrderItemService(final OrderItemRepository entityRepository, final OrderService orderService,
+                            final ProductService productService) {
         super(entityRepository);
+        this.orderService = orderService;
+        this.productService = productService;
     }
 
     @Override
@@ -25,5 +39,22 @@ public class OrderItemService extends AbstractModelService<OrderItem, OrderItemM
     @Override
     protected OrderItem getModelFromDto(final OrderItemModelDto dtoObject) {
         return new OrderItem(dtoObject.getQuantity());
+    }
+
+    @Override
+    public HttpStatus save(final Set<OrderItemModelDto> dto) {
+        final Function<OrderItemModelDto, OrderItem> insertDependenciesAndParseToModel = orderModelDto -> {
+            final Product productToInsert = productService.findByCodeOrThrowError(orderModelDto.getProductCode(),
+                    "SAVE PRODUCT");
+            final Order orderToInsert = orderService.findByCodeOrThrowError(orderModelDto.getOrderCode(),
+                    "SAVE ORDER");
+            final OrderItem orderItemToAdd = getModelFromDto(orderModelDto);
+            orderItemToAdd.setProduct(productToInsert);
+            orderItemToAdd.setOrder(orderToInsert);
+            return orderItemToAdd;
+        };
+
+        dto.stream().map(insertDependenciesAndParseToModel).forEach(entityRepository::save);
+        return HttpStatus.CREATED;
     }
 }
