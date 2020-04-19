8dc90d42c4f5631af5c997e996db8955d6a25569
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-04-19 16:36:43 +0200

    added @unique annotation

---
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/annotations/validators/Unique.java
@@ -0,0 +1,27 @@
+package pl.polsl.photoplus.annotations.validators;
+
+import pl.polsl.photoplus.services.controllers.FieldValueExists;
+import pl.polsl.photoplus.validators.UniqueValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = UniqueValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface Unique
+{
+    String message() default "{fieldNameToBeDisplayed} already in use.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    Class<? extends FieldValueExists> service();
+
+    String fieldName();
+
+    String fieldNameToBeDisplayed();
+}
