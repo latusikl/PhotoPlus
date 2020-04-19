824311adbfd22106aac1b4102bbc3cb117ceffe2
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-04-19 16:37:10 +0200

    added headers for edge

--- PhotoPlusFrontend/PhotoPlus/src/app/auth-interceptor.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/auth-interceptor.ts
@@ -7,14 +7,18 @@ export class AuthInterceptor implements HttpInterceptor {
               next: HttpHandler): Observable<HttpEvent<any>> {
 
         const token = localStorage.getItem("token");
-
+        /* for authorization in microsoft edge
+        req.headers.set('Access-Control-Allow-Origin', '*');
+        req.headers.set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token')
+        req.headers.set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        req.headers.set('Access-Control-Expose-Headers', '*');
+        */
         if (token != null) {
             console.log("Interceptor called.");
             const cloned = req.clone({
                 headers: req.headers.set("Authorization",
                     token)
             });
-
             return next.handle(cloned);
         } else {
             return next.handle(req);
