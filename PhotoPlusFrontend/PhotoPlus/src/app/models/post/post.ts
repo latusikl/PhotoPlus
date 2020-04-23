e5390ce70acd34c72e701dc1ec54ed549740b388
margawron <max2.marcel@gmail.com>
2020-04-23 18:43:33 +0200

    Initial commit for forum frontend models.

---
+++ PhotoPlusFrontend/PhotoPlus/src/app/models/post/post.ts
@@ -0,0 +1,6 @@
+export class Post {
+    date: Date;
+    topicCode: string;
+    content: string;
+    //? owner: usercode ?? string ?? User;
+}
---
+++ PhotoPlusFrontend/PhotoPlus/src/app/models/section/section.ts
@@ -0,0 +1,5 @@
+
+export class Section {
+    name: string;
+    description: string;
+}
