0312ec291f7ed17bc0d0e9cc761451c19b3a87b3
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-04-23 18:45:56 +0200

    formatting fix

--- PhotoPlusFrontend/PhotoPlus/src/app/services/login/login.service.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/login/login.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from "@angular/common/http";
-import {LoginModel} from "../../models/login/login-model.model";
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { LoginModel } from "../../models/login/login-model.model";
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
