aa57515017f7eef0de9e5da415fe14e7c28132c2
Lukasz Latusik <lukasz.latusik@sap.com>
2020-04-26 22:09:08 +0200

    Fixed dependency injection and reformated class.

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/security/custom/CustomPermissionEvaluator.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/security/custom/CustomPermissionEvaluator.java
@@ -4,25 +4,36 @@ import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
-import pl.polsl.photoplus.components.ContextProvider;
 import pl.polsl.photoplus.security.services.RolePropertiesService;
 
+import javax.annotation.PostConstruct;
 import java.io.Serializable;
 import java.util.List;
 
 @Service
-public class CustomPermissionEvaluator implements PermissionEvaluator {
+public class CustomPermissionEvaluator
+        implements PermissionEvaluator
+{
 
-    private final List<String> publicLinkWhitelist;
+    private final RolePropertiesService rolePropertiesService;
 
-    public CustomPermissionEvaluator() {
-        final RolePropertiesService rolePropertiesService = ContextProvider.getBean(RolePropertiesService.class);
+    private List<String> publicLinkWhitelist;
+
+    public CustomPermissionEvaluator(final RolePropertiesService rolePropertiesService)
+    {
+        this.rolePropertiesService = rolePropertiesService;
+    }
+
+    @PostConstruct
+    public void test()
+    {
         this.publicLinkWhitelist = rolePropertiesService.getPropertiesByRoleName("all");
     }
 
     @Override
-    public boolean hasPermission(final Authentication auth, final Object targetDomainObject, final Object permission) {
-        if ((auth == null) || (targetDomainObject == null) || !(permission instanceof String)){
+    public boolean hasPermission(final Authentication auth, final Object targetDomainObject, final Object permission)
+    {
+        if ((auth == null) || (targetDomainObject == null) || !(permission instanceof String)) {
             return false;
         }
 
@@ -30,8 +41,8 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
     }
 
     @Override
-    public boolean hasPermission(final Authentication auth, final Serializable targetId, final String targetType,
-                                 final Object permission) {
+    public boolean hasPermission(final Authentication auth, final Serializable targetId, final String targetType, final Object permission)
+    {
         if ((auth == null) || (targetType == null) || !(permission instanceof String)) {
             return false;
         }
@@ -39,11 +50,12 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
         return hasPrivilege(auth, targetType, permission.toString());
     }
 
-    private boolean hasPrivilege(final Authentication auth, final String targetType, final String permission) {
+    private boolean hasPrivilege(final Authentication auth, final String targetType, final String permission)
+    {
         final String urlToAccess = "/" + targetType + "/" + permission;
         for (final GrantedAuthority grantedAuth : auth.getAuthorities()) {
             if (grantedAuth.getAuthority().equalsIgnoreCase(urlToAccess) || publicLinkWhitelist.contains(urlToAccess)) {
-                    return true;
+                return true;
             }
         }
         return false;
