dc80800fdc03f1a4114d9ae5eae2255bfa324ea7
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-04-28 18:03:57 +0200

    hal links fix

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/PhotoplusApplication.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/PhotoplusApplication.java
@@ -1,11 +1,14 @@
 package pl.polsl.photoplus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HypermediaAutoConfiguration.class)
 @EnableScheduling
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PhotoplusApplication
