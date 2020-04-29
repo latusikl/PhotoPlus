94efd510b6fecd56f35128b9e9ef9298696a6369
margawron <max2.marcel@gmail.com>
2020-04-30 00:53:59 +0200

    Implement viewing topics for each section.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/forum/topic/header/topic-header.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/forum/topic/header/topic-header.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { TopicService } from 'src/app/services/topic/topic.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Topic } from 'src/app/models/topic/topic';
+import { UserService } from 'src/app/services/user/user.service';
+import { Observable, BehaviorSubject, Subject, VirtualTimeScheduler } from 'rxjs';
+import { User } from 'src/app/models/user/user';
 
 @Component({
   selector: 'app-topic-header',
@@ -7,9 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicHeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input("topic")
+  topic: Topic;
+  @Input("canModify")
+  canModify: boolean;
+  topicOwnerName: User;
+  
+  constructor(private userService:UserService, private topicService: TopicService, private activatedRoute:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
+    this.userService.getSingle(parseInt(this.topic.userCode)).subscribe(data=> {
+      this.topicOwnerName = data;
+    });
   }
-
 }
