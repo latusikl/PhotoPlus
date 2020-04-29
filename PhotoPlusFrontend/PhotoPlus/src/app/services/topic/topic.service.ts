59645df57b6594ab72c0cae3996c05ed1cf5a833
margawron <max2.marcel@gmail.com>
2020-04-29 23:23:56 +0200

    Implement method for getting all the topics in section.

--- PhotoPlusFrontend/PhotoPlus/src/app/services/topic/topic.service.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/topic/topic.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AbstractService } from '../abstract-service';
 import { Topic } from '../../models/topic/topic';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,9 @@ export class TopicService extends AbstractService<Topic> {
 
   constructor(http:HttpClient) { 
     super(http, "topic");
+  }
+
+  getAllFromSectionCode(sectionCode: number): Observable<Topic[]>{
+    return this._http.get<Topic[]>(this.hostAddress + this.endpointUrl + "/all?sectionCode=" + sectionCode);
   }
 }
