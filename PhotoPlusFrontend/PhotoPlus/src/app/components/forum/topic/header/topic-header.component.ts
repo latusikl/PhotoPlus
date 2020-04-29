4a05943ce19d559204308f1b9f24ae2dac3ee5d2
margawron <max2.marcel@gmail.com>
2020-04-30 01:02:18 +0200

    Show owner of the topic and moderators edit and remove buttons.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/forum/topic/header/topic-header.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/forum/topic/header/topic-header.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, Inject, Input } from '@angular/core';
-import { TopicService } from 'src/app/services/topic/topic.service';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Topic } from 'src/app/models/topic/topic';
 import { UserService } from 'src/app/services/user/user.service';
-import { Observable, BehaviorSubject, Subject, VirtualTimeScheduler } from 'rxjs';
 import { User } from 'src/app/models/user/user';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-topic-header',
@@ -15,15 +14,15 @@ export class TopicHeaderComponent implements OnInit {
 
   @Input("topic")
   topic: Topic;
-  @Input("canModify")
   canModify: boolean;
   topicOwnerName: User;
   
-  constructor(private userService:UserService, private topicService: TopicService, private activatedRoute:ActivatedRoute, private router:Router) { }
+  constructor(private userService:UserService, private loginService: LoginService, private activatedRoute:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
     this.userService.getSingle(parseInt(this.topic.userCode)).subscribe(data=> {
       this.topicOwnerName = data;
     });
+    this.canModify = this.loginService.getLoggedUser().login === this.topicOwnerName.login || this.loginService.isModerator;
   }
 }
