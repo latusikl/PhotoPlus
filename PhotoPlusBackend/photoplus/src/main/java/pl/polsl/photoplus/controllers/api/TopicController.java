29803f7f8ba15143f44097b9f192c0ba8b61e4ac
margawron <max2.marcel@gmail.com>
2020-04-30 02:35:21 +0200

    Implement custom endpoint returning code of created topic. Refactored insertDependenciesAndParseToModel function from TopicService

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/TopicController.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/TopicController.java
@@ -3,13 +3,11 @@ package pl.polsl.photoplus.controllers.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.polsl.photoplus.model.dto.TopicModelDto;
 import pl.polsl.photoplus.services.controllers.TopicService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -31,6 +29,13 @@ public class TopicController extends BaseModelController<TopicModelDto,TopicServ
         final List<TopicModelDto> dtos = this.dtoService.getTopicsBySection(sectionCode);
         addLinks(dtos);
         return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasPermission(this.authorizationPrefix, 'post' )")
+    public ResponseEntity<List<String>> post(@RequestBody @Valid final List<TopicModelDto> dtos){
+        final List<String> codesFromSavedDtos = this.dtoService.saveAndReturnSaved(dtos);
+        return new ResponseEntity(codesFromSavedDtos, HttpStatus.OK);
     }
 
     @Override
