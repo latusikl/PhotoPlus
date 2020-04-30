a81b129d7dbcce77d6b8728a3f0f8cac98ab7df9
margawron <max2.marcel@gmail.com>
2020-04-30 04:20:46 +0200

    Implement changing topics titles.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/forum/topic/edit/topic-edit.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/forum/topic/edit/topic-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TopicService } from 'src/app/services/topic/topic.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { Topic } from 'src/app/models/topic/topic';
 
 @Component({
   selector: 'app-topic-edit',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicEditComponent implements OnInit {
 
-  constructor() { }
+  topic:Topic;
+  topicForm:FormGroup;
+  submitted:boolean;
 
+  constructor(private formBuilder:FormBuilder,private activatedRoute: ActivatedRoute, private topicService: TopicService, private router:Router) { }
+  
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let topicCode = params["topicCode"];
+      this.topicService.getSingle(topicCode).subscribe(topicData => {  
+        this.topic = topicData;      
+        this.topicForm = this.formBuilder.group({
+          title: [topicData.name,Validators.required],
+        });
+      })
+    })
+    
   }
 
+  get f(){
+    return this.topicForm.controls;
+  }
+
+  async onSubmit(){
+    this.submitted = true;
+    if(this.topicForm.invalid){
+      return;
+    }
+    const form = this.topicForm.value;
+    this.topicService.patch(parseInt(this.topic.code),{...this.topic, name: form.title }).subscribe(()=>{
+      this.router.navigate(["/forum/section", this.topic.sectionCode]);
+    });
+  }
 }
