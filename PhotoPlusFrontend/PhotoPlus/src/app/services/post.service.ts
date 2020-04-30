38a086df6134184ba18d2ea1860d5668c2583ee7
margawron <max2.marcel@gmail.com>
2020-04-30 16:21:20 +0200

    Implement getting posts only from one topic.

--- PhotoPlusFrontend/PhotoPlus/src/app/services/post.service.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AbstractService } from './abstract-service';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/post/post';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,9 @@ export class PostService extends AbstractService<Post> {
 
   constructor(private http: HttpClient) { 
     super(http, "post");
+  }
+
+  getAllFromTopic(topicCode: number): Observable<Post[]>{
+    return this._http.get<Post[]>(this.hostAddress + this.endpointUrl + "/byTopic/" + topicCode);
   }
 }
