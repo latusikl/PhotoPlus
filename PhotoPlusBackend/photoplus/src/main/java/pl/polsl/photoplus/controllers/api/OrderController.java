7617103ad6c34ccf83f6b2932b005aeaf59a5401
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-04-30 16:10:27 +0200

    added @valid annotation to valid order items inside OrderModelDtoWithOrderItems

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/OrderController.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/OrderController.java
@@ -1,6 +1,5 @@
 package pl.polsl.photoplus.controllers.api;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/model/dto/OrderModelDtoWithOrderItems.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/model/dto/OrderModelDtoWithOrderItems.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class OrderModelDtoWithOrderItems extends OrderModelDto {
     }
 
     @JsonProperty("orderItems")
-    @NotEmpty(message = "OrderItem list shouldn't be empty.")
+    @NotEmpty(message = "Order item list shouldn't be empty.")
+    @Valid
     private List<OrderItemModelDto> orderItemModelDtos;
 }
