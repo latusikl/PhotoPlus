d43d354a414d6bb72efa11825c680d9145f419a8
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-01 03:16:37 +0200

    used parameter injection instead of application context

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/security/services/PermissionEvaluatorService.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/security/services/PermissionEvaluatorService.java
@@ -2,17 +2,18 @@ package pl.polsl.photoplus.security.services;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import pl.polsl.photoplus.model.entities.AbstractEntityModel;
 import pl.polsl.photoplus.model.entities.User;
 import pl.polsl.photoplus.repositories.UserRepository;
+import pl.polsl.photoplus.services.controllers.AbstractModelService;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class PermissionEvaluatorService {
+public class PermissionEvaluatorService<S extends AbstractModelService> {
 
     private final RolePropertiesService rolePropertiesService;
     private List<String> publicLinkWhitelist;
@@ -29,8 +30,7 @@ public class PermissionEvaluatorService {
         this.publicLinkWhitelist = rolePropertiesService.getPropertiesByRoleName("all");
     }
 
-    public boolean hasPrivilege(final String url, final String permission) {
-        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    public boolean hasPrivilege(final Authentication auth, final String url, final String permission) {
         final String urlToAccess = "/" + url + "/" + permission;
         for (final GrantedAuthority grantedAuth : auth.getAuthorities()) {
             if (grantedAuth.getAuthority().equalsIgnoreCase(urlToAccess) || publicLinkWhitelist.contains(urlToAccess)) {
@@ -40,12 +40,12 @@ public class PermissionEvaluatorService {
         return false;
     }
 
-    public boolean hasPrivilege(final String code) {
-        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    public boolean hasPrivilege(final Authentication auth, final String userCode) {
         final Optional<User> loggedUser = userRepository.findUserByLogin(auth.getPrincipal().toString());
         if (loggedUser.isPresent()) {
-            return code.equals(loggedUser.get().getCode());
+            return userCode.equals(loggedUser.get().getCode());
         }
         return false;
     }
+
 }
