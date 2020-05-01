d231099b5dc4bac96839e13e0a7cfe179f76c2fd
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-01 03:19:06 +0200

    Added getter because SpEL cannot use inherited field.  Added function to get post owner code.

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/PostController.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/PostController.java
@@ -3,10 +3,7 @@ package pl.polsl.photoplus.controllers.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.polsl.photoplus.model.dto.PostModelDto;
 import pl.polsl.photoplus.security.services.PermissionEvaluatorService;
 import pl.polsl.photoplus.services.controllers.PostService;
@@ -28,11 +25,18 @@ public class PostController extends BaseModelController<PostModelDto,PostService
     }
 
     @GetMapping(produces = {"application/json"})
-    @PreAuthorize("@permissionEvaluatorService.hasPrivilege(this.authorizationPrefix, 'all' )")
+    @PreAuthorize("@permissionEvaluatorService.hasPrivilege(authentication, this.authorizationPrefix, 'all' )")
     public ResponseEntity<List<PostModelDto>> getAllFromCategory(@RequestParam final String topicCode) {
         final List<PostModelDto> dtos = this.dtoService.getPostsByTopic(topicCode);
         addLinks(dtos);
         return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @PatchMapping("/editPost/{code}")
+    @PreAuthorize("@permissionEvaluatorService.hasPrivilege(authentication, this.getService().getPostOwnerCode(#code))")
+    public ResponseEntity patch(@RequestBody final PostModelDto dtoPatch, @PathVariable("code") final String code)
+    {
+        return new ResponseEntity(dtoService.patch(dtoPatch, code));
     }
 
     @Override
@@ -41,5 +45,13 @@ public class PostController extends BaseModelController<PostModelDto,PostService
         dto.add(linkTo(methodOn(PostController.class).delete(dto.getCode())).withRel(DELETE_RELATION_NAME));
         dto.add(linkTo(methodOn(TopicController.class).getSingle(dto.getTopicCode())).withRel(TOPIC_RELATION_NAME));
         dto.add(linkTo(methodOn(UserController.class).getSingle(dto.getUserCode())).withRel(CREATOR_RELATION_NAME));
+    }
+
+    /**
+     *
+     * You need to use getter to use service in @PreAuthorize because SpEL cannot use inherited field.
+     */
+    public PostService getService() {
+        return this.dtoService;
     }
 }
