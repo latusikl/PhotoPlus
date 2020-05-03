9c1d8c4bf5c92e45efd23b474c69a009a36a6e22
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-03 19:13:44 +0200

    added report controller

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/ReportController.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/ReportController.java
@@ -3,23 +3,24 @@ package pl.polsl.photoplus.controllers.api;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.polsl.photoplus.model.dto.BatchModelDto;
+import pl.polsl.photoplus.model.entities.Product;
 import pl.polsl.photoplus.services.controllers.BatchService;
 import pl.polsl.photoplus.services.controllers.CategoryService;
 import pl.polsl.photoplus.services.controllers.OrderService;
 import pl.polsl.photoplus.services.controllers.ProductService;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/report")
@@ -39,28 +40,54 @@ public class ReportController {
 
     @GetMapping(path = "/profit", produces = {"application/pdf"})
     @PreAuthorize("@permissionEvaluatorService.hasPrivilege(authentication, 'report', 'single' )")
-    public ResponseEntity<ByteArrayResource> getSingle() throws DocumentException {
-        ByteArrayResource resource = generateProfitReport();
+    public ResponseEntity<ByteArrayResource> getProfitReport(@RequestParam("beginDate")
+                                                             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                                 final LocalDate beginDate,
+                                                             @RequestParam("endDate")
+                                                             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                                final LocalDate endDate) throws DocumentException {
+        final ByteArrayResource resource = generateProfitReport(beginDate, endDate);
         return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
-    private ByteArrayResource generateProfitReport() throws DocumentException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        Document document = new Document();
+    private ByteArrayResource generateProfitReport(final LocalDate beginDate, final LocalDate endDate) throws DocumentException {
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final Document document = new Document();
         PdfWriter.getInstance(document, byteArrayOutputStream);
 
         document.open();
         Font font = FontFactory.getFont(FontFactory.COURIER, 20, BaseColor.BLACK);
         Chunk chunk = new Chunk("PROFIT REPORT", font);
-        Paragraph para1 = new Paragraph(chunk);
-        para1.setAlignment(Paragraph.ALIGN_CENTER);
-        para1.setSpacingAfter(50);
-        document.add(para1);
+        Paragraph para = new Paragraph(chunk);
+        para.setAlignment(Paragraph.ALIGN_CENTER);
+        para.setSpacingAfter(50);
+        document.add(para);
 
+        font = FontFactory.getFont(FontFactory.COURIER, 15, BaseColor.BLACK);
+        final Double profit = getProfit(beginDate, endDate);
+        chunk = new Chunk("Profit from " + beginDate + " to " + endDate + " : " + profit + "$", font);
+        para = new Paragraph(chunk);
+        para.setAlignment(Paragraph.ALIGN_JUSTIFIED);
+        para.setSpacingAfter(50);
+        document.add(para);
 
         document.close();
 
         return new ByteArrayResource(byteArrayOutputStream.toByteArray());
+    }
+
+    private Double getProfit(final LocalDate beginDate, final LocalDate endDate) {
+        Double profit = 0.0;
+        final List<BatchModelDto> batchList = batchService.getAll();
+        for (final BatchModelDto batch : batchList) {
+            if (batch.getDate().compareTo(beginDate) >= 0 && batch.getDate().compareTo(endDate) <= 0) {
+                profit -= batch.getSupplyQuantity() * batch.getPurchasePrice();
+                final Product product = productService.findByCodeOrThrowError(batch.getProductCode(),
+                        "GET PROFIT");
+                profit += (batch.getSupplyQuantity() - batch.getStoreQuantity()) * product.getPrice();
+            }
+        }
+        return profit;
     }
 
 
