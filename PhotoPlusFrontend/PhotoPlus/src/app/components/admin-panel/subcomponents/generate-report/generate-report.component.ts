336a4cbdc3cfa1c31549221955e9ce46be75fe6c
margawron <max2.marcel@gmail.com>
2020-05-04 22:12:24 +0200

    Fix conflicts.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/generate-report/generate-report.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/generate-report/generate-report.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ReportService } from 'src/app/services/report/report.service';
 
 @Component({
   selector: 'app-generate-report',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateReportComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("fromDate")
+  fromDateEl: ElementRef;
+
+  @ViewChild("toDate")
+  toDateEl: ElementRef;
+
+  constructor(private reportService: ReportService) { }
 
   ngOnInit(): void {
-  }
+    
 
+  }
+  generateProfitReport(){
+    const fromDate = this.fromDateEl.nativeElement.value as Date;
+    const toDate = this.toDateEl.nativeElement.value as Date;
+    console.log(fromDate, toDate, fromDate > toDate);
+    if(fromDate > toDate ){
+      alert("Starting date should not be greater than ending date");
+      return;
+    }
+    this.reportService.getProfitReport(fromDate, toDate).subscribe(pdfObject => {
+      const downloadURL = window.URL.createObjectURL(pdfObject);
+      const link = document.createElement("a");
+      link.href = downloadURL;
+      link.download = `${fromDate} ${toDate} profit report.pdf`;
+      link.click();
+    })
+  }
 }
--- PhotoPlusFrontend/PhotoPlus/src/app/services/report/report.service.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/report/report.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,7 @@ export class ReportService {
 
   constructor(private http:HttpClient) { }
 
-  
+  getProfitReport(fromDate: Date, toDate: Date): Observable<Blob>{
+    return this.http.get<Blob>(this.hostAddress + "report" + "/profit" + `?beginDate=${fromDate}&endDate=${toDate}`);
+  }
 }
