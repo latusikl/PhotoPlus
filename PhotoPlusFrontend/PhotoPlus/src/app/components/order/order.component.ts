34cf8808e74bdf5d708086e0a68cbedb6a3b6cbc
Tomek <juraszek.tomek@gmail.com>
2020-05-05 13:09:41 +0200

    Add address to order

--- PhotoPlusFrontend/PhotoPlus/src/app/components/order/order.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/order/order.component.ts
@@ -22,88 +22,99 @@ export class OrderComponent implements OnInit {
   items: [Product, number][];
   price: number;
   order: Order;
-  user: any=[];
+  user: any = [];
   paymentMethod: string;
-  isChecked: boolean=false;
-  error:any;
-  myDate=new Date();
+  isChecked: boolean = false;
+  error: any;
+  myDate = new Date();
   addreses: any[];
-  
-  constructor(private cartService: CartService,private http: HttpClient,private datePipe: DatePipe,private modalService: NgbModal,private loginService: LoginService) {
+
+  constructor(private cartService: CartService, private http: HttpClient, private datePipe: DatePipe, private modalService: NgbModal, private loginService: LoginService) {
     this.cartService.getSummaryPrice().subscribe(value => this.price = value);
-    this.order={
+    this.order = {
       userCode: 0,
       orderStatus: 'PENDING',
       paymentMethod: 'PAYPAL',
       price: 0,
       date: "2020-01-30",
-      orderItems: [{productCode: 0,quantity:0, orderCode:'orderCode'}]
+      orderItems: [{ productCode: 0, quantity: 0, orderCode: 'orderCode' }],
+      address: {
+        street: "street",
+        number: 0,
+        zipCode: "00000",
+        city: "city",
+        countryCode: "code",
+        userCode: 0,
+      }
     }
 
   }
 
-  FieldsChange(values:any){
-    this.isChecked=true;
-    this.paymentMethod=values.target.value
+  FieldsChange(values: any) {
+    this.isChecked = true;
+    this.paymentMethod = values.target.value
     console.log(this.paymentMethod)
-    }
+  }
 
   ngOnInit(): void {
 
-    this.http.get<HttpResponse<[]>>('http://localhost:8090/address/all',  
-    ).subscribe((data:any)=>{
-      console.log(data)
-      this.addreses=data
-      console.log("Adres")
-      console.log(this.addreses)
-    })
-    
+    if (this.loginService.isLoggedIn() == true) {
+      this.http.get<HttpResponse<[]>>('http://localhost:8090/address/all',
+      ).subscribe((data: any) => {
+        console.log(data)
+        this.addreses = data
+        console.log("Adres")
+        this.order.address.city = this.addreses[0].city
+        this.order.address.countryCode = this.addreses[0].country
+        this.order.address.number = this.addreses[0].number
+        this.order.address.street = this.addreses[0].street
+        this.order.address.userCode = this.addreses[0].userCode
+        this.order.address.zipCode = this.addreses[0].zipCode
+        console.log(this.order.address)
+      })
+    }
+
     this.items = this.cartService.getItems();
     console.log(this.items)
     this.items.forEach(element => {
-      this.order.orderItems.push({productCode: element[0].code,quantity:element[1],orderCode:'orderCode'})
+      this.order.orderItems.push({ productCode: element[0].code, quantity: element[1], orderCode: 'orderCode' })
     });
-    this.order.orderItems.splice(0,1)
+    this.order.orderItems.splice(0, 1)
     console.log(this.order)
-    this.user=localStorage.getItem("loggedUser")
-    this.user=JSON.parse(this.user)
-    if(this.loginService.isLoggedIn() == true)
-    {
-    this.order.userCode=this.user.code
+    this.user = localStorage.getItem("loggedUser")
+    this.user = JSON.parse(this.user)
+    if (this.loginService.isLoggedIn() == true) {
+      this.order.userCode = this.user.code
     }
-    this.order.price=this.price;
+    this.order.price = this.price;
     this.order.date = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');
     console.log([this.order])
   }
 
-  buy()
-  {
+  buy() {
 
-    if(this.loginService.isLoggedIn() == false)
-    {
-    const modalRef = this.modalService.open(ErrorModalComponent);
-    modalRef.componentInstance.title = "Error occured!";
-    modalRef.componentInstance.message = "Please login!.";
-    console.log("dziala")
-    return;
+    if (this.loginService.isLoggedIn() == false) {
+      const modalRef = this.modalService.open(ErrorModalComponent);
+      modalRef.componentInstance.title = "Error occured!";
+      modalRef.componentInstance.message = "Please login!.";
+      console.log("dziala")
+      return;
     }
 
-    if(this.isChecked==false)
-    { 
-      this.error=document.getElementById("error")
-      this.error.style.display="block"
+    if (this.isChecked == false) {
+      this.error = document.getElementById("error")
+      this.error.style.display = "block"
       return
     }
-    if(this.isChecked==true)
-    { 
-      this.error=document.getElementById("error")
-      this.error.style.display="none"
+    if (this.isChecked == true) {
+      this.error = document.getElementById("error")
+      this.error.style.display = "none"
     }
-    this.order.paymentMethod=this.paymentMethod
-    
-    this.http.post<HttpResponse<Order[]>>('http://localhost:8090/order/buy',  
-     [this.order]
-    ).subscribe(res => {})
+    this.order.paymentMethod = this.paymentMethod
+
+    this.http.post<HttpResponse<Order[]>>('http://localhost:8090/order/buy',
+      [this.order]
+    ).subscribe(res => { })
 
   }
 
--- PhotoPlusFrontend/PhotoPlus/src/app/models/order/order.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/models/order/order.ts
@@ -1,14 +1,22 @@
 import { Product } from '../product/product';
 
 export class Order {
-        userCode: number;
-        orderStatus: string;
-        paymentMethod: string;
-        price: number;
-        date: string;
-        orderItems:[{
-          orderCode: string;
-          productCode: number,
-          quantity: number
-        }]
+  userCode: number;
+  orderStatus: string;
+  paymentMethod: string;
+  price: number;
+  date: string;
+  orderItems: [{
+    orderCode: string;
+    productCode: number;
+    quantity: number;
+  }]
+  address: {
+    street: string;
+    number: number;
+    zipCode: string;
+    city: string;
+    countryCode: string;
+    userCode: number;
+  }
 }
