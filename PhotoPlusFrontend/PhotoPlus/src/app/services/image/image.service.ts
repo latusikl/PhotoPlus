3da54071796641f7866c70351b438c052ff30c3e
margawron <max2.marcel@gmail.com>
2020-05-06 04:14:56 +0200

    Create and implement image service.

---
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/image/image.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { ImageResponse } from "../../models/image/imageResponse";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  protected hostAddress = environment.hostAddress;
+
+  constructor(private httpClient: HttpClient) { }
+
+  postImage(file: File){
+    let formData = new FormData();
+    formData.append('file',file,file.name);
+    return this.httpClient.request<ImageResponse>('POST',this.hostAddress + "image",{body: formData});
+  };
+}
