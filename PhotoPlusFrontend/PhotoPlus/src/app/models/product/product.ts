1212edcff88ea5b10dfce733bb033a9edea9bdee
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-09 17:58:10 +0200

    changes in product model

--- PhotoPlusFrontend/PhotoPlus/src/app/models/product/product.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/models/product/product.ts
@@ -1,9 +1,10 @@
 import { Link } from '../link/link';
 
 export class Product {
-  code: number;
+  code: string;
   name: string;
   price: number;
+  storeQuantity: number;
   description: string;
   links: Link[]
   category: string;
