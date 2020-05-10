377c02c64fb9308a4660e61f193abcc05fa181e2
Tomek <juraszek.tomek@gmail.com>
2020-05-10 18:48:37 +0200

    logged user can buy

--- PhotoPlusFrontend/PhotoPlus/src/app/components/cart/cart.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/cart/cart.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart/cart.service';
 import { Product } from '../../models/product/product';
+import { LoginService } from 'src/app/services/login/login.service';
+import { ErrorModalComponent } from '../error-modal/error-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -12,7 +16,7 @@ export class CartComponent implements OnInit {
   items: [Product, number][];
   price: number;
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private loginService: LoginService, private modalService: NgbModal, private router: Router) {
     this.cartService.getSummaryPrice().subscribe(value => this.price = value);
   }
 
@@ -28,9 +32,19 @@ export class CartComponent implements OnInit {
     if (value > 0 && value < 100 && Number.isInteger(+value)) {
       this.cartService.changeQuantity(value, item);
     } else {
-    console.log(item[0].code);
-      (document.querySelector(("#input"+item[0].code).toString()) as HTMLInputElement).value = item[1].toString();
+      console.log(item[0].code);
+      (document.querySelector(("#input" + item[0].code).toString()) as HTMLInputElement).value = item[1].toString();
     }
+  }
+  buy() {
+    if (this.loginService.isLoggedIn() == false) {
+      const modalRef = this.modalService.open(ErrorModalComponent);
+      modalRef.componentInstance.title = "Error occured!";
+      modalRef.componentInstance.message = "Please login!.";
+      console.log("dziala")
+      return;
+    }
+    this.router.navigate(['/order']);
   }
 
 }
