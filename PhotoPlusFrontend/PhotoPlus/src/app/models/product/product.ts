dea8f9dca771e9ea6bdfeb7a43906a2628285197
margawron <max2.marcel@gmail.com>
2020-05-10 16:07:26 +0200

    Mark optional parameters as such, delete those which dont belong.

--- PhotoPlusFrontend/PhotoPlus/src/app/models/product/product.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/models/product/product.ts
@@ -3,13 +3,12 @@ import { Link } from '../link/link';
 export class Product {
   code: string;
   name: string;
-  price: number;
-  storeQuantity: number;
-  description: string;
-  quantity: number;
-  links: Link[]
   category: string;
-  imagesUrl: string[];
+  price: number;
+  description: string;
+  storeQuantity: number;
+  links?: Link[]
+  imagesUrl?: string[];
   imageCodes: string[];
-  dataLinks: Map<string, string>;
+  dataLinks?: Map<string, string>;
 }
