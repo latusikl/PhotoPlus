2d1d8aa4615d180af167e2616a64f88038ffdd96
margawron <max2.marcel@gmail.com>
2020-05-12 19:30:14 +0200

    Implement page fetching in abstract service.

--- PhotoPlusFrontend/PhotoPlus/src/app/services/abstract-service.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/abstract-service.ts
@@ -37,4 +37,7 @@ export abstract class AbstractService<T> {
     return this._http.get<PageInfo>(this.hostAddress + this.endpointUrl + "/all/page/count");
   }
 
+  public getPage(page:number): Observable<T[]>{
+    return this._http.get<T[]>(this.hostAddress + this.endpointUrl + "/all/" + page);
+  }
 }
