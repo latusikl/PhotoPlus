379eba22c610cb39650a597d4501cc82345205c5
margawron <max2.marcel@gmail.com>
2020-05-12 19:32:51 +0200

    Eliminate not needed parts of page-select component.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/page-select/page-select.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/page-select/page-select.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 
 
@@ -10,15 +10,12 @@ interface PageAmount{
 @Component({
   selector: 'app-page-select',
   templateUrl: './page-select.component.html',
-  styleUrls: ['./page-select.component.scss']
+  styleUrls: ['./page-select.component.scss'],
 })
 export class PageSelectComponent implements OnInit {
 
   @Input("amountOfPages")
   amountOfPages: BehaviorSubject<number>;
-
-  @Input("selectedPage")
-  selectedPage: BehaviorSubject<number>;
 
   @Output("onPageSelect")
   onPageSelect= new EventEmitter<number>();
@@ -29,9 +26,6 @@ export class PageSelectComponent implements OnInit {
   }
 
   pageSelect(pageNumber: any){
-    if(this.selectedPage.value === pageNumber){
-      return;
-    }
     this.onPageSelect.next(pageNumber.pageIndex as number);
   }
 
