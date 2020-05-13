39735cbcae97234768466fd9207ebd73680a95cc
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-13 19:05:36 +0200

    pagination on products

--- PhotoPlusFrontend/PhotoPlus/src/app/components/products/products.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/products/products.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../models/product/product';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { SuccessModalComponent } from '../success-modal/success-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BehaviorSubject } from 'rxjs';
 
 
 @Component({
@@ -13,15 +14,35 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[];
+  products: BehaviorSubject<Product>[];
+
+  selectedPage: BehaviorSubject<number>;
+  amountOfPages: BehaviorSubject<number>;
+
 
   constructor(private productService: ProductService, private cartService: CartService, private modalService: NgbModal) { }
 
-  ngOnInit(): void {
-    this.productService.getAll().subscribe((data: Product[]) => {
-      this.products = data;
-      this.products.forEach(element => { this.productService.getDataFromLinks(element) });
+  async ngOnInit() {
+    this.selectedPage = new BehaviorSubject(0);
+    this.amountOfPages = new BehaviorSubject(0);
+    let pageInfo = this.productService.getPageCount().toPromise();
+    this.loadProducts();
+    let info = await pageInfo;
+    this.amountOfPages.next((await pageInfo).pageAmount);
+  }
+
+  loadProducts(){
+    this.products = new Array<BehaviorSubject<Product>>();
+    this.productService.getPage(this.selectedPage.value).subscribe((data) => {
+      for (let product of data) {
+        this.products.push(new BehaviorSubject(product));
+      }
     });
+  }
+
+  changePage(page:number){
+    this.selectedPage.next(page);
+    this.loadProducts();
   }
 
   addToCart(product: Product) {
