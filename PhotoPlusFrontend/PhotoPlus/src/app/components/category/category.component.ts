1df32bb170e471d97dec624324821fcc8feab00e
Suchar230 <dsj230@gmail.com>
2020-05-13 20:41:57 +0200

    quotations fix

--- PhotoPlusFrontend/PhotoPlus/src/app/components/category/category.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/category/category.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CategoryService } from "../../services/category/category.service";
+import { CategoryService } from '../../services/category/category.service';
 import { Category } from '../../models/category/category';
-import { ProductService } from "../../services/product/product.service";
+import { ProductService } from '../../services/product/product.service';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { Product } from '../../models/product/product';
 import { SuccessModalComponent } from '../success-modal/success-modal.component';
@@ -42,7 +42,7 @@ export class CategoryComponent implements OnInit {
   addToCart(product: Product) {
     this.cartService.addToCart(product);
     const modalRef = this.modalService.open(SuccessModalComponent);
-    modalRef.componentInstance.message = "Please go to checkout to place an order.";
-    modalRef.componentInstance.title = "Added " + product.name + " to card.";
+    modalRef.componentInstance.message = 'Please go to checkout to place an order.';
+    modalRef.componentInstance.title = 'Added ' + product.name + ' to card.';
   }
 }
