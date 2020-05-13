f877ad04fdb90a0379a1eb50fdabcf9357d23dcf
Suchar230 <dsj230@gmail.com>
2020-05-13 20:53:12 +0200

    quotations fix

--- PhotoPlusFrontend/PhotoPlus/src/app/components/registration/registration.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/registration/registration.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { MustMatch } from '../../helpers/must-match';
 import { User } from '../../models/user/user';
-import { UserService } from "../../services/user/user.service";
+import { UserService } from '../../services/user/user.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ErrorModel } from 'src/app/models/error/errormodel';
 import { ErrorModalComponent } from '../error-modal/error-modal.component';
@@ -20,10 +20,9 @@ export class RegistrationComponent implements OnInit {
   registerForm: FormGroup;
   submitted: boolean;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService,
-    private modalService: NgbModal) {
-
-   }
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService,
+              private modalService: NgbModal) {}
 
   ngOnInit(): void {
    this.registerForm = this.formBuilder.group({
@@ -33,7 +32,7 @@ export class RegistrationComponent implements OnInit {
     confirmPass : ['', [Validators.required]],
     name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
     surname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
-    phoneNumber: ['',  Validators.pattern(new RegExp("[1-9][0-9]{2}-[0-9]{3}-[0-9]{3}"))]
+    phoneNumber: ['',  Validators.pattern(new RegExp('[1-9][0-9]{2}-[0-9]{3}-[0-9]{3}'))]
    }, {
      validator: [MustMatch('password', 'confirmPass')]
     });
@@ -61,8 +60,8 @@ export class RegistrationComponent implements OnInit {
 
     this.userService.post(user).subscribe(result => {
       const modalRef = this.modalService.open(SuccessModalComponent);
-      modalRef.componentInstance.message = "Welcome " + user.name + " " + user.surname + "!";
-      modalRef.componentInstance.title = "Registered successfully!";
+      modalRef.componentInstance.message = 'Welcome ' + user.name + ' ' + user.surname + '!';
+      modalRef.componentInstance.title = 'Registered successfully!';
     });
   }
 
