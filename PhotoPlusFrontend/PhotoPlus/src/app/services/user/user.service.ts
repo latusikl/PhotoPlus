3788149f4904c4b25c4efa84724bc0c183afdfc5
margawron <max2.marcel@gmail.com>
2020-05-14 01:49:07 +0200

    Add search by login endpoint.

--- PhotoPlusFrontend/PhotoPlus/src/app/services/user/user.service.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/services/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../models/user/user';
 import { AbstractService } from '../abstract-service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class UserService extends AbstractService<User> {
 
   constructor(http: HttpClient) {
     super(http, 'user')
+  }
+
+  getUsersSearchByLogin(searchText:string): Observable<User[]>{
+    return this._http.get<User[]>(this.hostAddress + this.endpointUrl + "/search/" + searchText);
   }
 
 }
