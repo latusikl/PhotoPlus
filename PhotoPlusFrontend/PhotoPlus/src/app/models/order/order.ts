8b304c7f5861e47646fcee510efb83057520f048
margawron <max2.marcel@gmail.com>
2020-05-16 02:04:16 +0200

    Use order status enum to represent it in order dto.

--- PhotoPlusFrontend/PhotoPlus/src/app/models/order/order.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/models/order/order.ts
@@ -1,10 +1,11 @@
 import { PaymentMethod } from '../payment-method/payment-method';
+import { OrderStatus } from '../order-status/order-status';
 
 export class Order{
   code?: string;
   userCode: string;
   addressCode: string;
-  orderStatus: string;
+  orderStatus: OrderStatus;
   paymentMethod: PaymentMethod;
   date: Date;
   price: number;
