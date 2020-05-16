f77a76cad7c8535e04944baee01d73d9997b2413
margawron <max2.marcel@gmail.com>
2020-05-16 05:49:41 +0200

    Implement loading an order.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/manage-single-order/manage-single-order.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/manage-single-order/manage-single-order.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { OrderService } from "src/app/services/order/order.service";
+import { Order } from "src/app/models/order/order";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
-  selector: 'app-manage-single-order',
-  templateUrl: './manage-single-order.component.html',
-  styleUrls: ['./manage-single-order.component.scss']
+  selector: "app-manage-single-order",
+  templateUrl: "./manage-single-order.component.html",
+  styleUrls: ["./manage-single-order.component.scss"],
 })
 export class ManageSingleOrderComponent implements OnInit {
 
-  constructor() { }
+  order: BehaviorSubject<Order>;
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private orderService: OrderService
+  ) {}
+
+  ngOnInit() {
+    this.order = new BehaviorSubject({} as Order);
+    this.loadOrder();
   }
 
+  async loadOrder() {
+    this.activatedRoute.params.subscribe((params) => {
+      this.orderService.getSingle(params["orderCode"]).subscribe((x) => {
+        this.order.next(x);
+      });
+    });
+  }
 }
