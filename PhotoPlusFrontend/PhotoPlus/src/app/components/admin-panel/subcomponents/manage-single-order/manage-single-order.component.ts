9c29a9073a50095aaad2898513541abc35c0c7e6
margawron <max2.marcel@gmail.com>
2020-05-16 06:09:25 +0200

    Implement single order viewer in admin panel. TODO: Allow modifying order state.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/manage-single-order/manage-single-order.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/manage-single-order/manage-single-order.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from "@angular/router";
 import { OrderService } from "src/app/services/order/order.service";
 import { Order } from "src/app/models/order/order";
 import { BehaviorSubject } from "rxjs";
+import { User } from 'src/app/models/user/user';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: "app-manage-single-order",
@@ -12,14 +14,20 @@ import { BehaviorSubject } from "rxjs";
 export class ManageSingleOrderComponent implements OnInit {
 
   order: BehaviorSubject<Order>;
+  user: BehaviorSubject<User>;
+//address: BehaviorSubject<Address>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private userService: UserService,
+    // private addressService: AddressService // TODO wait for someone xD
   ) {}
 
   ngOnInit() {
     this.order = new BehaviorSubject({} as Order);
+    this.user = new BehaviorSubject({} as User);
+  //this.address = new BehaviorSubject({} as Address);  
     this.loadOrder();
   }
 
@@ -27,7 +35,27 @@ export class ManageSingleOrderComponent implements OnInit {
     this.activatedRoute.params.subscribe((params) => {
       this.orderService.getSingle(params["orderCode"]).subscribe((x) => {
         this.order.next(x);
+        this.loadUser();
+        this.loadAddress();
       });
     });
   }
+
+  async loadUser(){
+    this.user.next(await this.userService.getSingle(this.order.value.userCode).toPromise());
+  }
+
+  async loadAddress(){
+  //const address = this.addressService.getSingle(this.order.value.addressCode).toPromise;
+  //this.address.next(address);
+  }
+  
+  beatutifyEnum(paymentMethod:string){
+    if(!paymentMethod){
+      return;
+    }
+    const noUnderscore = paymentMethod.replace(/\_/g, " ");
+    return noUnderscore.charAt(0).toUpperCase() + noUnderscore.slice(1).toLowerCase();
+  }
+
 }
