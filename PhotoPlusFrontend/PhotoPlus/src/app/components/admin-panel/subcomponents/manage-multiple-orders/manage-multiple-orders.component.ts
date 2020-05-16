0a5139bd01c9719c462bc92723e192d8b8b82936
margawron <max2.marcel@gmail.com>
2020-05-16 04:53:49 +0200

    Basic order display.

--- PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/manage-multiple-orders/manage-multiple-orders.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/admin-panel/subcomponents/manage-multiple-orders/manage-multiple-orders.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { OrderService } from 'src/app/services/order/order.service';
 import { OrderStatus } from 'src/app/models/order-status/order-status';
 import { PageInfo } from 'src/app/models/page-info/page-info';
+import { Order } from 'src/app/models/order/order';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage-orders',
@@ -14,11 +16,16 @@ export class ManageMultipleOrdersComponent implements OnInit {
   // a wartości kluczy to wielkości stron poszczególnych OrderStatusów
   // klucze są opcjonalne aby wartości można było dodawać pojedynczo 
   orderStatusPageInfo: {[key in OrderStatus]?:PageInfo };
+  selectedPagesNumbers: {[key in OrderStatus]?: number}
+  currentStatusesOrders: {[key in OrderStatus]?: BehaviorSubject<Order[]>}
+
 
   constructor(private orderService: OrderService) { }
 
   async ngOnInit() {
     this.loadOrderStatusPagesInfo();
+    this.setDefaultPageNumbers();
+    this.loadSelectedPages();
   }
 
   async loadOrderStatusPagesInfo(){
@@ -29,6 +36,27 @@ export class ManageMultipleOrdersComponent implements OnInit {
     }
   }
 
+  setDefaultPageNumbers(){
+    this.selectedPagesNumbers = {};
+    for(const status in OrderStatus){
+      this.selectedPagesNumbers[status] = 0;
+    }
+  }
+
+  async loadSelectedPages(){
+    this.currentStatusesOrders = {};
+    for(const status in OrderStatus){
+      const pageNumer = this.selectedPagesNumbers[status];
+      let statusPage = this.orderService.getPageByOrderStatus(pageNumer, status).toPromise();
+      this.currentStatusesOrders[status] = new BehaviorSubject(await statusPage);      
+    }
+  }
+
+  goToSingleOrder(orderCode:string){
+    console.log(orderCode);
+    
+  }
+
   humanReadable(text: string): string{
     const noUnderscores = text.replace(/\_/g, " ")
     return noUnderscores.charAt(0) + noUnderscores.slice(1).toLowerCase();
@@ -36,6 +64,10 @@ export class ManageMultipleOrdersComponent implements OnInit {
 
   toCssClass(orderStatus: string): string{
     return orderStatus.toLowerCase() + '_bg_color';
+  }
+
+  changePage(orderStatus: OrderStatus, page: string){
+    console.log("change page", orderStatus, page);
   }
 
   get orderStatuses(): Array<string>{
