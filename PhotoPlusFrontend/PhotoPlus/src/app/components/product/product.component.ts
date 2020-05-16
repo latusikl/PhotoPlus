484a3907f143b6f3bdcbe08fb105b028cbb5689c
Suchar230 <dsj230@gmail.com>
2020-05-16 19:45:06 +0200

    code cleanup

--- PhotoPlusFrontend/PhotoPlus/src/app/components/product/product.component.ts
+++ PhotoPlusFrontend/PhotoPlus/src/app/components/product/product.component.ts
@@ -55,7 +55,7 @@ export class ProductComponent implements OnInit {
     });
     this.categoryService.getAll().subscribe(data => {
       this.categories = data;
-    })
+    });
     this.linksForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       link: ['', [Validators.required, Validators.pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)]],
