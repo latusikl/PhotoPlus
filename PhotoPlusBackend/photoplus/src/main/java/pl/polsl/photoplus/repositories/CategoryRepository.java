21ed01757b1d49f67e03a3bd9f100b8409a8677f
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-18 20:22:46 +0200

    sort category by name

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/repositories/CategoryRepository.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/repositories/CategoryRepository.java
@@ -3,6 +3,9 @@ package pl.polsl.photoplus.repositories;
 import org.springframework.stereotype.Repository;
 import pl.polsl.photoplus.model.entities.Category;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends EntityRepository<Category, Long> {
+    List<Category> findAllByOrderByName();
 }
