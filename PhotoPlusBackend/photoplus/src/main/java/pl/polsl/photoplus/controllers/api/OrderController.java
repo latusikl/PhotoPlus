6fd03369ec64acf01841a7decaa6d799e3dd71d5
Lukasz Latusik <lukasz.latusik@sap.com>
2020-05-19 00:16:42 +0200

    Added endpoint to get order codes for given user.

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/OrderController.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/controllers/api/OrderController.java
@@ -4,7 +4,13 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pl.polsl.photoplus.model.dto.OrderModelDto;
 import pl.polsl.photoplus.model.dto.OrderModelDtoWithOrderItems;
 import pl.polsl.photoplus.model.enums.OrderStatus;
@@ -12,24 +18,26 @@ import pl.polsl.photoplus.security.services.PermissionEvaluatorService;
 import pl.polsl.photoplus.services.controllers.OrderService;
 
 import javax.validation.Valid;
-
 import java.util.List;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("/order")
-public class OrderController extends BaseModelController<OrderModelDto,OrderService> {
+public class OrderController
+        extends BaseModelController<OrderModelDto,OrderService>
+{
 
     private final String CUSTOMER_RELATION_NAME = "customer";
 
-    public OrderController(final OrderService dtoService, final PermissionEvaluatorService permissionEvaluatorService) {
+    public OrderController(final OrderService dtoService, final PermissionEvaluatorService permissionEvaluatorService)
+    {
         super(dtoService, "order", permissionEvaluatorService);
     }
 
     @Override
-    public void addLinks(final OrderModelDto dto) {
+    public void addLinks(final OrderModelDto dto)
+    {
         dto.add(linkTo(methodOn(OrderController.class).getSingle(dto.getCode())).withSelfRel());
         dto.add(linkTo(methodOn(OrderController.class).delete(dto.getCode())).withRel(DELETE_RELATION_NAME));
         dto.add(linkTo(methodOn(UserController.class).getSingle(dto.getUserCode())).withRel(CUSTOMER_RELATION_NAME));
@@ -37,19 +45,26 @@ public class OrderController extends BaseModelController<OrderModelDto,OrderServ
 
     @PostMapping("/buy")
     @PreAuthorize("@permissionEvaluatorService.hasPrivilege(authentication, this.authorizationPrefix, 'post' )")
-    public ResponseEntity saveOrderWithOrderItems(@RequestBody @Valid final OrderModelDtoWithOrderItems dto) {
+    public ResponseEntity saveOrderWithOrderItems(@RequestBody @Valid final OrderModelDtoWithOrderItems dto)
+    {
         return new ResponseEntity(dtoService.saveWithItems(dto));
     }
 
-
     @GetMapping(path = "/{page}", produces = "application/json", params = "orderStatus")
     @PreAuthorize("@permissionEvaluatorService.hasPrivilege(authentication, this.authorizationPrefix, 'all' )")
-    public ResponseEntity<List<OrderModelDto>> getAll(@PathVariable("page") final Integer page,
-                                                      @RequestParam final OrderStatus orderStatus)
+    public ResponseEntity<List<OrderModelDto>> getAll(@PathVariable(
+            "page") final Integer page, @RequestParam final OrderStatus orderStatus)
     {
         final List<OrderModelDto> dtos = dtoService.getPage(page, orderStatus);
         addLinks(dtos);
         return new ResponseEntity<>(dtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/byUser/{userCode}")
+    @PreAuthorize("@permissionEvaluatorService.hasPrivilege(authentication,#userCode)")
+    public ResponseEntity<List<String>> getOrderBasicByUser(final @PathVariable("userCode") String userCode)
+    {
+        return dtoService.getOrderBasicFromUser(userCode);
     }
 
     @GetMapping(path = "/page/count", produces = "application/json", params = "orderStatus")
