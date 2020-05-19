dcd0a9d4f7a9f56a0222e14fa7fe575fb6258030
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-19 21:45:19 +0200

    get all by product code method

--- PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/repositories/OrderItemRepository.java
+++ PhotoPlusBackend/photoplus/src/main/java/pl/polsl/photoplus/repositories/OrderItemRepository.java
@@ -13,4 +13,5 @@ public interface OrderItemRepository extends EntityRepository<OrderItem, Long> {
     List<OrderItem> getAllByOrder_Code(final String code);
     Page<OrderItem> findAllByOrder_Code(Pageable page, final String code);
     OrderItem findFirstByOrder_Code(String code);
+    List<OrderItem> findAllByProduct_Code(final String code);
 }
